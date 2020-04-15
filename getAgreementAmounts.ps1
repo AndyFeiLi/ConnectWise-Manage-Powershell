df9d30d508d14d163091199844d42338cf485ee9
AndyFeiLi <59521017+AndyFeiLi@users.noreply.github.com>
2020-04-15 15:27:25 +0800

    Delete getAgreementAmounts.ps1

--- getAgreementAmounts.ps1
+++
@@ -1,9 +0,0 @@
-$agree = Get-CWMAgreement -all
-
-#$alladditions = Get-CWMAgreementAddition  683
-
-foreach($entry in $agree){
-
-	$additions = Get-CWMAgreementAddition  $entry.id
-	$alladditions = $alladditions + $additions
-}
